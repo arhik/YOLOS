02585ba39fd905705b1800b71c6c337f3f775bba
arhik <karthik.katipalli@gmail.com>
2021-07-21 08:29:07 +0530

    backcompatibility version check bug

--- util/misc.py
+++ util/misc.py
@@ -18,7 +18,7 @@ from torch import Tensor
 
 # needed due to empty tensor bug in pytorch and torchvision 0.5
 import torchvision
-if float(torchvision.__version__[:3]) < 0.7:
+if list(map(int, torchvision.__version__.split('.')[:2])) < list(map(int, "0.7".split(".")[:2])):
     from torchvision.ops import _new_empty_tensor
     from torchvision.ops.misc import _output_size
 
